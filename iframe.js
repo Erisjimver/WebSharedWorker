20d3069da3c3d27d7a27b0361c96915719e7d1aa
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-06-06 10:42:49 -0500

    Nota
    El uso de esta API debe ser ejecutada en un servidor, o al menos usando WAMP usando un ordenador como servidor local.

---
+++ iframe.js
@@ -0,0 +1,22 @@
+function comenzar(){
+
+	trabajador = new SharedWorker("trabajador_compartido.js");
+
+	trabajador.port.addEventListener("message",recibido,false);
+
+	trabajador.port.start();
+
+}
+
+
+
+function recibido(e){
+
+	zonadatos=document.getElementById("zonadatos");
+
+	zonadatos.innerHTML=e.data;
+
+}
+
+
+window.addEventListener("load", comenzar,false);
